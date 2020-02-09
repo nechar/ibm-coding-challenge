00aa19e0a333df94463c09d2bcaac4790e5cf3a1
Nechar <joshinechar@gmail.com>
2020-02-09 18:17:40 +1100

    feature: testing setup

---
+++ src/modules/items/items.controller.spec.ts
@@ -0,0 +1,22 @@
+import { ItemsController } from './items.controller';
+import { ItemsService } from '../../services';
+
+describe('ItemsController', () => {
+  let itemsService: ItemsService;
+  let itemsController: ItemsController;
+
+  beforeEach(() => {
+    itemsService = new ItemsService();
+    itemsController = new ItemsController(itemsService);
+  });
+
+  describe('save', () => {
+    it('should be able to save an item', async () => {
+      const response = await itemsController.save({
+        code: 'Vegemite Scroll',
+        name: 'VS',
+      });
+      expect(response.success).toBeTruthy();
+    });
+  });
+});
